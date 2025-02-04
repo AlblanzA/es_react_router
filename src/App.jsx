c4300b8bd638febbfca59f20ee1acd399fa4f288
Alberto Lanzafame <lanzafamea2001@gmail.com>
2025-02-04 03:19:54 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,29 @@
-/* eslint-disable react/jsx-no-undef */
- 
-import { AlertClock } from "./AlertClock"
-import { MouseClicker } from "./MouseClicker"
- 
-import { MyForm } from "./MyForm"
-import { MyList } from "./MyList"
-// eslint-disable-next-line no-unused-vars
-import { MyUncontrolledForm } from "./MyUncontrolledForm"
-import { Welcome } from "./Welcome"
-import { LanguageContext } from "./LanguageContex"
-import { useState } from "react"
-import { GithubUser } from "./GithubUser"
-import { Route } from "react-router-dom"
+import { Routes, Route, Link } from 'react-router-dom';
+import Welcome from './Welcome';
+import Counter from './Counter';
+import GithubUserList from './GithubUserList';
+import ShowGithubUser from './ShowGithubUser';
 
-//compongo piu componenti, creo un componente che utilizza jsx 
-//per eseguire il rendering di un altro componente 
-//creo quindi un albero di componenti
-export function App() {
+function App() {
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/counter">Counter</Link>
+        <Link to="/users">Users</Link>
+      </nav>
 
-    const [Language, setLanguage] = useState('en')
-
-    function handleSetLanguage(language){
-          setLanguage(language)
-    }
-
-    return (
-        <Container title={<div>
-            <h1>My Awesome App</h1>
-             </div>}>
-             <div>
-                <Link to="/">Home</Link> 
-                <Link to="products"> Products</Link>
-             </div>
-            <Routes>
-                <Route path="/:name" element={<Welcome />}/>
-                <Route path="products" element={<Catalogue/>} />
-                  <Route index element = {<p>Please selecet a product</p>}/>
-                 <Route path=":id" element={<Product/>} />
-            </Routes>
-            <Route path="*" element={<div> <p>Not found</p></div>}/>
-            <Link to="/">Go Home</Link>
-        </Container>
-
-    )
+      <Routes>
+        <Route path="/" element={<Welcome name="John" />} />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="/users" element={<GithubUserList />}>
+          <Route path=":username" element={<ShowGithubUser />} />
+          <Route index element={<p>Add a user and select it</p>} />
+        </Route>
+        <Route path="*" element={<h1>404 - Not Found</h1>} />
+      </Routes>
+    </div>
+  );
 }
+
+export default App;
---
+++ src/GithubUserList.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+import { Link, Outlet } from 'react-router-dom';
+
+function GithubUserList() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users')
+      .then(response => response.json())
+      .then(data => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      <h2>GitHub Users</h2>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            <Link to={`/users/${user.login}`}>{user.login}</Link>
+          </li>
+        ))}
+      </ul>
+      <Outlet />
+    </div>
+  );
+}
+
+export default GithubUserList;
